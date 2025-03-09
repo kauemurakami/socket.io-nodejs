3fe73dd82016b0e47308a40de3177ccc296fff59
Kauê <kauetmurakami@gmail.com>
2025-03-08 21:23:48 -0300

    Add instruction to tests with postman Finish project test

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+### Socket.io app
+
+The basic implementation od the socket.io
+
+### Tests Postman
+Currently, socket.io collection sharing is not yet available [issue](https://github.com/postmanlabs/postman-app-support/issues/11252), so I will leave the prints of how I tested it below.<br/>
+
+Open Postman and on your collection, on the icon + click and select the option View More templates, look for websocket, select and that's it<br/>
+
+![connection](postman/event%20connection.png) <br/>
+This applies to all events, just change the event name in send, except the word event.<br/>
+
+Word event<br/>
+![word](postman/event%20word.png)<br/>
+
+Add listener for `result`<br/>
+![result](postman/listen%20result.png)
 
--- index.js
+++ index.js
@@ -2,22 +2,26 @@ var express = require('express')
 var app = express()
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
-const jsonToObject = require('./core/middlewares/json_to_object')
+const jsonToObjectMiddleware = require('./core/middlewares/json_to_object')
 
 //nome do evento //socket = client
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
   // console.log(socket)
   // console.log(socket.id)
 
   //middleware para converter automaticamente os jsons em objetos
-  jsonToObject(socket)
+  jsonToObjectMiddleware(socket)
 
-  socket.on('hello', (data)=>{
-    // console.log(```hello ${data}```)
-    console.log('hello'+ data.name)
+  socket.on('disconnect', () => {
+    console.log('X disconnected ' + socket.id)
   })
 
-  socket.on('word', (data)=>{
+  socket.on('hello', (data) => {
+    // console.log(```hello ${data}```)
+    console.log('hello' + data.name)
+  })
+
+  socket.on('word', (data) => {
     socket.emit('result', 'User ' + data.user)
   })
 
@@ -25,7 +29,7 @@ io.on('connection', (socket)=>{
 })
 
 
-http.listen(3000, ()=>{
-  console.log('hello world')
+http.listen(3000, () => {
+  console.log('APP RUNNING PORT:3000')
 })
 
